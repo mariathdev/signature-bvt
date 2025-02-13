6fc4bc2ee0ba2bf08c1fe41fb5df94487cbd85e2
mariathdev <mariath.dev@outlook.com>
2025-02-13 20:57:36 -0300

    commit test

---
+++ public/index.js
@@ -0,0 +1,46 @@
+console.log("Start")
+
+const inputFields = document.querySelectorAll('input');
+inputFields.forEach((inputField, index) => {
+	inputField.addEventListener("keydown", function (event) {
+		if (event.key == "Enter") {
+			event.preventDefault();
+			const nextTexinputField = inputFields[index + 1];
+			if (nextTexinputField != null)
+				nextTexinputField.focus();
+			else
+				trySubmit();
+		}
+	})
+});
+
+const formHasErrors = () => {
+	inputFields.forEach((inputField) => {
+		if (inputField.value == "")
+			alert("Não podem haver campos vazios.");
+			return (false);
+	})
+	if (inputFields[0].value.length > 20) {
+		alert("O nome deve ter 20 caracteres ou menos.");	
+		return (false);
+	}
+	if (!inputFields[2].value.includes("@bravante.com.br")) {
+		alert("O email não é @bravante.com.br");	
+		return (false);
+	}
+	return (true);
+}
+
+const trySubmit = () => {
+	console.log("Tried submitting");
+	if (formHasErrors())
+		return (false);
+	
+	console.log(inputFields[0].value);
+	console.log(inputFields[1].value);
+	console.log(inputFields[2].value);
+	console.log(inputFields[3].value);
+
+}
+
+console.log("End")
---
+++ server.js
@@ -0,0 +1,43 @@
+console.log("START -> server.js Initialization");
+
+// 0. Initialization
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3300
+
+// 1. Uses
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/assets", express.static(path.join(__dirname, "assets")));
+app.use(express.static("public"));
+
+// 2. Route to serve main index.html
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// 3. API
+
+const signatureGenerator = () => {
+	const pythonProcess = spawn("python3", ["signatureGenerator.py", inputFields[0], inputFields[1], inputFields[2], inputFields[3]]);
+	let imageData = Buffer.alloc(0);
+	pythonProcess.stdout.on("data", (data) => {
+		imageData = Buffer.concat([imageData, data]);
+	});
+	pythonProcess.on("close", () => {
+		callback(null, imageData);
+	});
+	pythonProcess.stderr.on("data", (data) => {
+		console.error("Erro no Python:", data.toString());
+		callback(new Error("Erro ao gerar a imagem"));
+	});
+};
+
+signatureGenerator("Mariacci", "Dev", "Mariacci@bravante.com.br", "14125116114")
+
+// 4. Start Server
+app.listen(PORT, '0.0.0.0', () => {
+	console.log(`Main Server running at port: ${PORT}`);
+});
+
+console.log("END   -> server.js Initialization");
